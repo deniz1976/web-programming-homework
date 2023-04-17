077a68fa8b30ec720437f620a535f6c54927270b
Deniz Mutlu <96434352+deniz1976@users.noreply.github.com>
2023-04-17 20:56:38 +0300

    Update chart.js
    temperature part is created and stored to localStorage so producer and consumer see the same temperature at the same time

--- phpodev/chart.js
+++ phpodev/chart.js
@@ -1,8 +1,7 @@
 new Morris.Line({
-    // ID of the element in which to draw the chart.
+    
     element: 'myfirstchart',
-    // Chart data records -- each entry in this array corresponds to a point on
-    // the chart.
+    
     lineColors: ['orange'],
     data: [
         { year: '2019', value: 1.576 },
@@ -12,12 +11,11 @@ new Morris.Line({
         { year: '2023', value: 0.350 }
 
     ],
-    // The name of the data record attribute that contains x-values.
+    
     xkey: 'year',
-    // A list of names of data record attributes that contain y-values.
+    
     ykeys: ['value'],
-    // Labels for the ykeys -- will be displayed when you hover over the
-    // chart.
+    
     labels: ['Value']
 });
 Morris.Donut({
@@ -32,3 +30,35 @@ Morris.Donut({
     labelColor: '#ffffff',
     colors: ['orange', '#0072f2', '#f42a26', '#87cefa', '#32cd32']
 });
+//degree part
+let kitchenDegree = parseInt(localStorage.getItem('kitchenDegree')) || 25; // default değer 25
+document.getElementById('kitchenDegree').innerHTML = kitchenDegree + '°C';
+
+
+document.getElementById('hot').addEventListener('click', function () {
+    if (kitchenDegree < 30) {
+        kitchenDegree++;
+        document.getElementById('kitchenDegree').innerHTML = kitchenDegree + '°C';
+        localStorage.setItem('kitchenDegree', kitchenDegree);
+    }
+    else {
+        kitchenDegree = 30;
+        document.getElementById('kitchenDegree').innerHTML = kitchenDegree + '°C';
+        localStorage.setItem('kitchenDegree', kitchenDegree);
+    }
+});
+
+
+document.getElementById('cold').addEventListener('click', function () {
+    if (kitchenDegree > 17) {
+        kitchenDegree--;
+        document.getElementById('kitchenDegree').innerHTML = kitchenDegree + '°C';
+        localStorage.setItem('kitchenDegree', kitchenDegree);
+    }
+    else {
+        kitchenDegree = 17;
+        document.getElementById('kitchenDegree').innerHTML = kitchenDegree + '°C';
+        localStorage.setItem('kitchenDegree', kitchenDegree);
+    }
+});
+
