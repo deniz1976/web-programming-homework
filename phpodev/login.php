9f73ddf6adcf2842a2f49281a5a2dca4ab3f4e79
Deniz Mutlu <96434352+deniz1976@users.noreply.github.com>
2023-04-17 18:46:07 +0300

    Update login.php
    error http class created

--- phpodev/login.php
+++ phpodev/login.php
@@ -1,56 +1,57 @@
-
+<!DOCTYPE html>
 <html>
-<head>Log In</head>
+<head>
+	<link rel="stylesheet" type="text/css" href="loginscreen.css">
+</head>
 <body>
-    <form action="login.php" method="post">
-        <input type="text" name="username" placeholder="Username">
-        <input type="password" name="password" placeholder="Password">
-        <input type="submit" name="submit" value="Log In">
+	<div class="header">
+		<h1>Welcome to home automation system</h1>
+		<h2>Please log in</h2>
+	</div>
+
+	<form action="login.php" method="post">
+		<input type="text" name="username" placeholder="Username">
+		<input type="password" name="password" placeholder="Password">
+		<input type="submit" name="submit" value="Log In">
+	</form>
+	
+	<?php
+		$producers = ["producer1","producer2"];
+		$producerPasswords = ["1881","1923"];
+		$consumers = ["consumer1","consumer2"];
+		$consumerPasswords = ["1920","1938"];
+		$message ="";
+		$user = false;
+		echo "<script>localStorage.setItem('user', false)</script>";
+
+		if(isset($_POST["submit"])){
+			$username = $_POST['username'];
+			$password = $_POST['password'];
+			if($message != ""){
+				$message ="<br>";
+				echo $message;
+			}
+			else{
+				if($password == $producerPasswords[0] && $username == $producers[0] || $password == $producerPasswords[1] && $username == $producers[1]){
+					echo "<script>localStorage.setItem('user', true)</script>";
+					echo "<script>window.location.href='producerApp.html'</script>";
+					exit();
+				}
+				else if($password == $consumerPasswords[0] && $username == $consumers[0] || $password == $consumerPasswords[1] && $username == $consumers[1]){
+					echo "<script>localStorage.setItem('user', true)</script>";
+					echo "<script>window.location.href='consumerApp.html'</script>";
+					exit();
+				}
+				else if($username == "" && $password == ""){
+					echo "<div class='error'>Please enter username and password.</div>";
+					exit();
+				}
+				else{
+					echo "<div class='error'>Wrong username or password.</div>";
+					exit();
+				}  
+			}
+		}
+	?>
 </body>
 </html>
-
-<?php
-$producers = ["producer1","producer2"];
-$producerPasswords = ["1881","1923"];
-$consumers = ["consumer1","consumer2"];
-$consumerPasswords = ["1920","1938"];
-$message ="";
-$user = false;
-echo "<script>localStorage.setItem('user', false)</script>";
-
-if(isset($_POST["submit"])){
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    if($message != ""){
-        $message ="<br>";
-        echo $message;
-    }
-    else{
-    if($password == $producerPasswords[0] && $username == $producers[0] || $password == $producerPasswords[1] && $username == $producers[1]){
-        echo "<script>localStorage.setItem('user', true)</script>";
-        echo "<script>window.location.href='producerApp.html'</script>";
-            
-        
-}
-    else if($password == $consumerPasswords[0] && $username == $consumers[0] || $password == $consumerPasswords[1] && $username == $consumers[1]){
-        echo "<script>localStorage.setItem('user', true)</script>";
-        echo "<script>window.location.href='consumerApp.html'</script>";
-        
-    }
-    else if($username == "" && $password == ""){
-        $message = "<br>Enter username and password.";
-        echo $message;
-        
-    }
-    else    {
-        $message = "<br>Incorrect username or password.";
-        echo $message;
-        
-        }  
-    exit();
-    }
-}
-
-?>
-
-
